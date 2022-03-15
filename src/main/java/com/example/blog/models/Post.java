d2ad96f0db73616105047377107aa821cde5cad3
Zhandos Medetov <zhandosmedetov@gmail.com>
2022-03-16 01:01:01 +0600

    ll5 - post request, form in html, add data to mysql

--- src/main/java/com/example/blog/models/Post.java
+++ src/main/java/com/example/blog/models/Post.java
@@ -1,11 +1,16 @@
 package com.example.blog.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -13,43 +18,11 @@ public class Post {
     private String title, anons, full_text;
     private int views;
 
-    public Long getId() {
-        return id;
-    }
+    public Post() {}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public Post(String title, String anons, String full_text) {
         this.title = title;
-    }
-
-    public String getAnons() {
-        return anons;
-    }
-
-    public void setAnons(String anons) {
         this.anons = anons;
-    }
-
-    public String getFull_text() {
-        return full_text;
-    }
-
-    public void setFull_text(String full_text) {
         this.full_text = full_text;
-    }
-
-    public int getViews() {
-        return views;
-    }
-
-    public void setViews(int views) {
-        this.views = views;
     }
 }
