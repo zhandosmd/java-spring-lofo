cc314b72784ea61423982627d10c7f4d4172b347
Zhandos Medetov <zhandosmedetov@gmail.com>
2022-03-18 20:53:54 +0600

    ll7 - delete & edit post, end of lessons

--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -5,10 +5,7 @@ import com.example.blog.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -48,11 +45,46 @@ public class BlogController {
         if(!postRepository.existsById(id)){
             return "redirect:/blog";
         }
-
         Optional<Post> post = postRepository.findById(id);
         ArrayList<Post> res = new ArrayList<>();
         post.ifPresent(res::add);
         model.addAttribute("post", res);
         return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(
+        @PathVariable(value = "id") long id,
+        @RequestParam String title,
+        @RequestParam String place,
+        @RequestParam String description,
+        Model model)
+    {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setPlace(place);
+        post.setDescription(description);
+        postRepository.save(post);
+
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
     }
 }
--- src/main/java/com/example/blog/models/Post.java
+++ src/main/java/com/example/blog/models/Post.java
@@ -11,18 +11,18 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
-public class Post {
+public class Post { // model
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String title, anons, full_text;
+    private String title, place, description;
     private int views;
 
     public Post() {}
 
-    public Post(String title, String anons, String full_text) {
+    public Post(String title, String place, String description) {
         this.title = title;
-        this.anons = anons;
-        this.full_text = full_text;
+        this.place = place;
+        this.description = description;
     }
 }
